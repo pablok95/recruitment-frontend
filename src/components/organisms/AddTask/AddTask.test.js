3a3318774d85c2d39ddc376540a1f4feb09d20bf
Paweł Kotoniak <kotoniak@gmail.com>
2023-10-22 20:17:51 +0200

    feat: add new test

--- src/components/organisms/AddTask/AddTask.test.js
+++ src/components/organisms/AddTask/AddTask.test.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { validationSchema } from 'components/organisms/AddTask/index';
+import AddTask, { validationSchema } from 'components/organisms/AddTask/index';
+import {Provider} from "react-redux";
+import {render as rtlRender, screen} from '@testing-library/react';
+import store from "store";
+
+const render = () => {
+  const Wrapper = ({children}) => <Provider store={store}>{children}</Provider>
+  return rtlRender(<AddTask />, {wrapper: Wrapper})
+}
 
 describe('Add task form', () => {
+  beforeEach(() => {
+    render()
+  })
+
+  test('add task should be in the document', () => {
+    const titleLabel = screen.getByLabelText("Title*")
+    expect(titleLabel).toBeInTheDocument()
+  })
+
+  test("title field should have label", () => {
+    const emailInputNode = screen.getByLabelText("Title*");
+    expect(emailInputNode.getAttribute("name")).toBe("title");
+  });
+
   test('validate function should pass on correct input ', async () => {
     const exampleData = {
       title: 'Example task title',
